73d255516c52fbcbdb6b8b7c1f0962aca87253da
sasha <berezhalex90@gmail.com>
2024-10-27 12:11:55 +0300

    calculate location in viewModel

--- app/src/main/java/com/vendetta/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/vendetta/weather/presentation/MainActivity.kt
@@ -1,27 +1,16 @@
 package com.vendetta.weather.presentation
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
-import com.google.android.gms.tasks.CancellationToken
-import com.google.android.gms.tasks.CancellationTokenSource
-import com.google.android.gms.tasks.OnTokenCanceledListener
 import com.vendetta.weather.ui.content.WeatherScreen
 import com.vendetta.weather.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val permissions = arrayOf(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
 
     private val viewModel by viewModels<MainViewModel>()
 
@@ -32,25 +21,7 @@ class MainActivity : ComponentActivity() {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        if (checkLocationPermission()) {
-            fusedLocationProviderClient.getCurrentLocation(
-                Priority.PRIORITY_HIGH_ACCURACY,
-                object : CancellationToken() {
-                    override fun onCanceledRequested(p0: OnTokenCanceledListener) =
-                        CancellationTokenSource().token
-
-                    override fun isCancellationRequested() = false
-                }
-            ).addOnSuccessListener {
-                if (it != null) {
-                    viewModel.loadWeather(it)
-                } else {
-                    showDialogueWindow()
-                }
-            }
-        } else {
-            showDialogueWindow()
-        }
+        viewModel.getWeather(this, fusedLocationProviderClient)
 
         setContent {
             WeatherTheme {
@@ -59,21 +30,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-
-    private fun checkLocationPermission(): Boolean {
-        return (ContextCompat.checkSelfPermission(
-            this,
-            permissions[0]
-        ) == PackageManager.PERMISSION_GRANTED)
-    }
-
-    private fun showDialogueWindow() {
-        requestPermissions(permissions, RC_LOCATION)
-    }
-
-    companion object {
-        private const val RC_LOCATION = 101
     }
 }
 
--- app/src/main/java/com/vendetta/weather/presentation/MainViewModel.kt
+++ app/src/main/java/com/vendetta/weather/presentation/MainViewModel.kt
@@ -1,11 +1,22 @@
 package com.vendetta.weather.presentation
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 import com.vendetta.weather.R
 import com.vendetta.weather.data.repository.WeatherRepositoryImpl
 import com.vendetta.weather.domain.entity.WeatherEntity
@@ -16,22 +27,59 @@ import java.util.Calendar
 
 class MainViewModel : ViewModel() {
 
+    private val repository = WeatherRepositoryImpl()
+
     private val _weatherEntity = MutableLiveData<WeatherEntity>()
     val weatherEntity: LiveData<WeatherEntity>
         get() = _weatherEntity
 
-    private val repository = WeatherRepositoryImpl()
-
     private val getWeatherInCurrentLocationTodayUseCase =
         GetWeatherInCurrentLocationTodayUseCase(repository)
 
-    fun loadWeather(location: Location) {
+    private fun loadWeather(location: Location) {
         viewModelScope.launch {
             _weatherEntity.value = getWeatherInCurrentLocationTodayUseCase(location)
             Log.i(
-                "Weather-Response",
-                getWeatherInCurrentLocationTodayUseCase(location).toString()
+                "Weather-Response", getWeatherInCurrentLocationTodayUseCase(location).toString()
             )
+        }
+    }
+
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    private fun checkLocationPermission(context: Context): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            context,
+            permissions[0]
+        ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun showDialogueWindow(activity: Activity) {
+        ActivityCompat.requestPermissions(activity, permissions, RC_LOCATION)
+    }
+
+    fun getWeather(
+        activity: Activity,
+        fusedLocationProviderClient: FusedLocationProviderClient
+    ) {
+        if (checkLocationPermission(activity)) {
+            fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY,
+                object : CancellationToken() {
+                    override fun onCanceledRequested(p0: OnTokenCanceledListener) =
+                        CancellationTokenSource().token
+
+                    override fun isCancellationRequested() = false
+                }).addOnSuccessListener {
+                if (it != null) {
+                    loadWeather(it)
+                } else {
+                    showDialogueWindow(activity)
+                }
+            }
+        } else {
+            showDialogueWindow(activity)
         }
     }
 
@@ -69,5 +117,9 @@ class MainViewModel : ViewModel() {
     }
 
     fun getYear() = Calendar.getInstance().get(Calendar.YEAR).toString()
+
+    companion object {
+        const val RC_LOCATION = 101
+    }
 
 }
